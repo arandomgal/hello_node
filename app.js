b9dc5cfbcfe592e0636a7bda88830de9bcd72ecc
arandomgal <michelle.andrews31313@gmail.com>
2016-07-07 15:58:43 -0400

    change port to 8080

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ var server = http.createServer(function (request, response) {
     response.write("Hello there! Current time is " + now);
     response.end();
 });
-server.listen(3333);
+server.listen(8080);
 console.log("Server is running at http://localhost:3333");
